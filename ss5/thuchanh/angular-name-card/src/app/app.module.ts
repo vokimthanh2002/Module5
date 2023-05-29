5e84cd590edd0388d6c51ab39efbfb4c18db4a5c
vokimthanh2002 <vothanh6081@gmail.com>
2023-05-29 17:56:40 +0700

    update

---
+++ ss5/thuchanh/angular-name-card/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NameCardComponent } from './name-card/name-card.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { RatingBarComponent } from './rating-bar/rating-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NameCardComponent,
+    ProgressBarComponent,
+    RatingBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
